f2d1b6e5af8dcf4f7b27a048b94b8a68ce578bd5
choedo <96doyeong@naver.com>
2025-11-20 18:46:39 +0900

    포스트 생성/수정/삭제 시 캐시 조작 로직 추가

--- src/hooks/mutations/post/use-update-post.ts
+++ src/hooks/mutations/post/use-update-post.ts
@@ -1,12 +1,30 @@
 import { updatePost } from "@/api/post";
-import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Post, UseMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useUpdatePost(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: updatePost,
-    onSuccess: () => {
+    onSuccess: (updatedPost) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Post>(
+        QUERY_KEYS.post.byId(updatedPost.id),
+        (prevPost) => {
+          if (!prevPost)
+            throw new Error(
+              `${updatedPost.id}에 해당하는 포스트 캐시 데이터를 찾을 수 없습니다.`,
+            );
+
+          return {
+            ...prevPost,
+            ...updatedPost,
+          };
+        },
+      );
     },
     onError: (error) => {
       console.error(error);
