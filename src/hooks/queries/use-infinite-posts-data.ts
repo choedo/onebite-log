3edca31ee3bfc0a0ad42d758e813a3417718f2ce
choedo <96doyeong@naver.com>
2025-11-20 17:01:12 +0900

    포스트 무한 스크롤 구현하기

---
+++ src/hooks/queries/use-infinite-posts-data.ts
@@ -0,0 +1,27 @@
+import { fetchPosts } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+const PAGE_SIZE = 5;
+
+export function useInfinitePostsData() {
+  return useInfiniteQuery({
+    queryKey: QUERY_KEYS.post.list,
+    queryFn: async ({ pageParam }) => {
+      const from = pageParam * PAGE_SIZE;
+      const to = from + PAGE_SIZE - 1;
+
+      const posts = await fetchPosts({ from, to });
+      return posts;
+    },
+
+    initialPageParam: 0,
+    getNextPageParam: (
+      lastPage, // 가장 마지막에 불러온 페이지
+      allPages, // 현재 불러온 데이터들의 모든 값 (페이지별 배열로 저장된다 2차원배열)
+    ) => {
+      if (lastPage.length < PAGE_SIZE) return undefined;
+      return allPages.length;
+    }, // 새로운 페이지를 불러와야할 때 다음 페이지를 계산하기 위한 옵션
+  });
+}
